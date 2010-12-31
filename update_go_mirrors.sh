da2bd862c4c95159158ad0ec6239d75d80cee4b6
Raymond S Brand <rsbx@acm.org>
2010-12-31 14:44:36 -0500

    Miscellaneous files.

---
+++ update_go_mirrors.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+REPOS="go gofrontend"
+
+for REPO in ${REPOS}; do
+	( cd ${REPO}.hg \
+			&& hg incoming \
+			&& hg pull \
+			&& hg update \
+	)
+	( cd ${REPO}.git \
+			&& ../fast-export.git/hg-fast-export.sh \
+			&& git-push git@github.com:rsbx/${REPO}.git
+	)
+done
