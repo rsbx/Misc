52c36b32a7e51a94f255e47365da9e072eced222
Raymond S Brand <rsbx@acm.org>
2011-01-06 11:31:52 -0500

    Silence git-fast-import and add some decorations to the script output.

--- update_go_mirrors.sh
+++ update_go_mirrors.sh
@@ -3,13 +3,16 @@
 REPOS="go gofrontend"
 
 for REPO in ${REPOS}; do
+	        echo '---------------------------------------------------------------------'
 	( cd ${REPO}.hg \
 			&& hg incoming \
 			&& hg pull \
 			&& hg update \
 	) \
 	&& ( cd ${REPO}.git \
-			&& ../fast-export.git/hg-fast-export.sh \
+			&& ../fast-export.git/hg-fast-export.sh --quiet \
 			&& git-push --all git@github.com:rsbx/${REPO}.git
 	)
 done
+echo '---------------------------------------------------------------------'
+date
